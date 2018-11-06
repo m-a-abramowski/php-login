b96e299af1d872dd13386a5ab50e4a3096457d85
Maciek Abramowski <m.abramowski@wp.pl>
2018-11-07 00:38:00 +0100

    typos fixed

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -22,7 +22,7 @@ if (isset($_POST['login-submit'])) {
 
       mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
       mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_results($stmt);
+      $result = mysqli_stmt_get_result($stmt);
       if ($row = mysqli_fetch_assoc($result)) {
         $pwdCheck = password_verify($password, $row['pwdUsers']);
         if ($pwdCheck == false){
--- index.php
+++ index.php
@@ -8,10 +8,10 @@ require "header.php";
         <section class="section-default">
          <?php 
             if (isset($_SESSION['userId'])) {
-             echo '<p>You are logged in!</p>'   
+             echo '<p>You are logged in!</p>';  
             }
             else {
-              echo '<p>You are logged out!</p>'  
+              echo '<p>You are logged out!</p>'; 
             }
             
             ?>
