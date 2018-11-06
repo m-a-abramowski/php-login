6e5fe4c517a002af31f52386b0d2110c9345ef52
Maciek Abramowski <m.abramowski@wp.pl>
2018-11-07 00:46:52 +0100

    logout

--- header.php
+++ header.php
@@ -23,15 +23,25 @@ session_start();
           <li> <a href="#">Contact with bullshit</a></li>
         </ul>
         <div class="">
-          <form action="includes/login.inc.php" method="post">
-            <input type="text" name="mailuid" placeholder="Username/E-mail...">
-            <input type="password" name="pwd" placeholder="Password...">
-            <button type="submit" name="login-submit">Login</button>
-          </form>
-          <a href="signup.php">Signup</a>
-          <form action="includes/logout.inc.php" method="post">
+         
+          
+          <?php 
+            if (isset($_SESSION['userId'])) {
+             echo '<form action="includes/logout.inc.php" method="post">
             <button type="submit" name="logout-submit">Logout</button>
-          </form>
+            </form>';  
+            }
+            else {
+              echo '<form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>'; 
+            }
+            
+            ?>
+          
         </div>
       </nav>
 
